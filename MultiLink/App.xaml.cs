d06a0049d7a2424f19ff49e886d8bd81306e3383
chiefmb <chief.mb1976@gmail.com>
2019-08-28 12:07:14 +0430

    added manual download

--- MultiLink/App.xaml.cs
+++ MultiLink/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using System.Windows;
 
 namespace MultiLink
@@ -13,5 +8,17 @@ namespace MultiLink
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var x = e.Args;
+            if (x.Length > 0)
+            {
+                var links = MultiLinkBuilder.Builder.BuildMultiLink(e.Args[0], int.Parse(e.Args[1]), int.Parse(e.Args[2]));
+                foreach (var link in links)
+                {
+                    Process.Start(link);
+                }
+            }
+        }
     }
 }
--- MultiLink/Views/Win.xaml.cs
+++ MultiLink/Views/Win.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+
 namespace MultiLink.Views
 {
     /// <summary>
@@ -15,6 +19,7 @@ namespace MultiLink.Views
 
         private void BtnGenerate_Click(object sender, RoutedEventArgs e)
         {
+            SpLinks.Children.Clear();
             foreach (var link in MultiLinkBuilder.Builder.BuildMultiLink(TxtBaseUrl.Text, int.Parse(TxtStartNum.Text), int.Parse(TxtEndNum.Text)))
             {
                 SpLinks.Children.Add(new TextBox { Text = link });
@@ -27,6 +32,55 @@ namespace MultiLink.Views
             {
                 Process.Start(item.Text);
             }
+        }
+
+        private async void BtnDl_Click(object sender, RoutedEventArgs e)
+        {
+            MpbDl.Visibility = Visibility.Visible;
+            MpbDl.Maximum = 1;
+            var l = SpLinks.Children.Count;
+            var i = 1;
+            foreach (TextBox item in SpLinks.Children)
+            {
+                MpbDl.Value = i * 1.0 / l;
+                var path = Path.Combine(TxtDlPath.Text, item.Text.Substring(item.Text.LastIndexOf("/") + 1));
+                var link = item.Text;
+                await Task.Run(() =>
+                {
+                    try
+                    {
+                        Download(link, path);
+                    }
+                    catch
+                    {
+                        Dispatcher.Invoke(() =>
+                        {
+                            item.Background = new SolidColorBrush(Colors.DarkRed);
+                            item.Foreground = new SolidColorBrush(Colors.White);
+                        });
+                    }
+                });
+                i++;
+            }
+            MpbDl.Visibility = Visibility.Collapsed;
+        }
+        private int dlStack = 0;
+        private void Download(string link, string path)
+        {
+            dlStack++;
+            var client = new System.Net.WebClient();
+            try
+            {
+                client.DownloadFile(link, path);
+            }
+            catch
+            {
+                if (dlStack < 4)
+                    Download(link, path);
+                else
+                    throw;
+            }
+            dlStack--;
         }
     }
 }
